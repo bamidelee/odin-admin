ce7266f3199966ab367b442714ec276af1166f11
bamidele <ibrahimolujide@gmail.com>
2023-09-27 19:23:39 +0100

    new

--- src/components/quarries.js
+++ src/components/quarries.js
@@ -774,16 +774,16 @@ export const CREATE_MUSIC = gql`
 `
 
 export const CREATE_MOVIE = gql`
-    mutation createMovie($description: String!, $title: String!, $primaryMedia: String!, $secondaryMedia: String!, $language: String!, $stars: [String!], $releaseDate: Date!, $genre: [String!], $source: String, $country: String!, $director: String, $trailer: String, $request: String){
-        createMovie(description: $description, title: $title, primaryMedia: $primaryMedia, secondaryMedia: $secondaryMedia,language: $language, stars: $stars, releaseDate: $releaseDate, genre: $genre, source: $source, country: $country, director: $director, trailer: $trailer, request: $request){
+    mutation createMovie($description: String!, $title: String!, $primaryMedia: String!, $secondaryMedia: String!, $language: String!, $stars: [String!], $releaseDate: Date!, $genre: [String!], $source: String, $country: String!, $director: String, $trailer: String, $request: String!){
+        createMovie(description: $description, title: $title, primaryMedia: $primaryMedia, secondaryMedia: $secondaryMedia,language: $language, stars: $stars, releaseDate: $releaseDate, genre: $genre, source: $source, country: $country, director: $director, trailer: $trailer, request: $request!){
             title
         }
     }
 `
 
 export const CREATE_SERIES = gql`
-    mutation createSeries($description: String!, $title: String!, $primaryMedia: String!, $secondaryMedia: String!, $language: String!, $stars: [String!], $releaseDate: Date!, $genre: [String!], $source: String, $season: String!, $episode: String!, $episodeTitle: String!, $country: String!, $director: String, $trailer: String, $request: String){
-        createSeries(description: $description, title: $title, primaryMedia: $primaryMedia, secondaryMedia: $secondaryMedia,language: $language, stars: $stars, releaseDate: $releaseDate, genre: $genre, source: $source, season: $season, episode: $episode, episodeTitle: $episodeTitle country: $country, director: $director, trailer: $trailer, request: $request){
+    mutation createSeries($description: String!, $title: String!, $primaryMedia: String!, $secondaryMedia: String!, $language: String!, $stars: [String!], $releaseDate: Date!, $genre: [String!], $source: String, $season: String!, $episode: String!, $episodeTitle: String!, $country: String!, $director: String, $trailer: String, $request: String!){
+        createSeries(description: $description, title: $title, primaryMedia: $primaryMedia, secondaryMedia: $secondaryMedia,language: $language, stars: $stars, releaseDate: $releaseDate, genre: $genre, source: $source, season: $season, episode: $episode, episodeTitle: $episodeTitle country: $country, director: $director, trailer: $trailer, request: $request!){
             title
         }
     }
